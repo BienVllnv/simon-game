8b2f653ff4110629b22af0ccd6543ef3f0afed8b
BienVllnv <75823701+BienVllnv@users.noreply.github.com>
2021-01-19 17:51:12 +0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,54 @@
+var gamePattern = [];
+var userClickedPattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var level = 0;
+var started = false;
+
+$(document).keydown(function(){
+    if (!started){
+        $("h1").text("Level " + level);
+        nextSequence();
+        started = true;
+    }   
+});
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userChosenColour);
+});
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+
+    level++;
+}
+
+function playSound (name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel){
+    if () {
+        console.log("wrong");
+    }
+    else{
+        console.log("success");
+    }    
+}
